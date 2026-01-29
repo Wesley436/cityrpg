fff1486fa8efcd2a9cc98d9672645cb111836007
WesleyHui <72075816+Wesley436@users.noreply.github.com>
2026-01-29 18:10:54 -0500

    simple api testing

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Alert } from "react-native";
+import axios from "axios";
 
 export default function Index() {
+  const testApi = async () => {
+      const response = await axios.get("http://ec2-54-235-233-32.compute-1.amazonaws.com/");
+      Alert.alert(response.data);
+  };
+
+
   return (
     <View
       style={{
@@ -9,7 +16,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity onPress={testApi}>
+          <Text>Test Node.js Endpoint</Text>
+      </TouchableOpacity>
     </View>
   );
 }
